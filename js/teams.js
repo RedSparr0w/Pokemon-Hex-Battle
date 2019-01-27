b479a477bdbb91575a2cea4d19d2f0ff7258018a
RedSparr0w <admin@redsparr0w.com>
2019-01-27 22:29:07 +1300

    make everything global..

--- js/teams.js
+++ js/teams.js
@@ -7,33 +7,33 @@ function cube_to_axial(x, z){
 }
 //test
 function getCellFromXZ(x,z){
-	return board.grid.cells[cube_to_axial(x,z)]
+	return board.grid.cells[cube_to_axial(x,z)].tile
 }
 
-function getPlayerBases(boardSize, players){
+function getPlayerBases(){
   corners = [
-      getCellFromXZ(cube_to_axial(0, -boardSize)),
-      getCellFromXZ(cube_to_axial(boardSize, -boardSize)),
-      getCellFromXZ(cube_to_axial(boardSize, 0)),
-      getCellFromXZ(cube_to_axial(0, boardSize)),
-      getCellFromXZ(cube_to_axial(-boardSize, boardSize)),
-      getCellFromXZ(cube_to_axial(-boardSize, 0)),
+      getCellFromXZ(0, -board.grid.size),
+      getCellFromXZ(board.grid.size, -board.grid.size),
+      getCellFromXZ(board.grid.size, 0),
+      getCellFromXZ(0, board.grid.size),
+      getCellFromXZ(-board.grid.size, board.grid.size),
+      getCellFromXZ(-board.grid.size, 0),
     ]
   if (players === 1){
     // center of map
     return [
-        {base:getCellFromXZ(cube_to_axial(0, 0))},
+        {base:getCellFromXZ(0, 0)},
       ];
   } else if (players === 2){
     return [
-        {base:corners[0]},
-        {base:corners[3]},
+        {base:corners[0], rotation: 0},
+        {base:corners[3], rotation: ((360 / 6) * 3) * vg.DEG_TO_RAD},
       ];
   } else if (players === 3){
     return [
-        {base:corners[0]},
-        {base:corners[2]},
-        {base:corners[4]},
+        {base:corners[0], rotation: 0},
+        {base:corners[2], rotation: ((360 / 6) * 2) * vg.DEG_TO_RAD},
+        {base:corners[4], rotation: ((360 / 6) * 4) * vg.DEG_TO_RAD},
       ];
   } else if (players === 4){
     return [
